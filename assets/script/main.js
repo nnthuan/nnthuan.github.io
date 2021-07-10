e2f241277ab9e6231f172eb428e9557d0e18062b
nguyennhuthuanse <nguyennhuthuanse@gmail.com>
2021-07-10 21:26:42 +0700

    refactor(main.js)

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,33 +1,61 @@
-const renderTopicButton = (topic) => {
-  let button = document.createElement("a");
-  button.className = "btn";
-  button.innerHTML = topic.content;
-  button.href = topic.url;
-  return button;
+//#region DataObject
+const TopicItemDataObject = {
+  contnet: "",
+  url: "",
+};
+//#endregion
+
+//#region Component
+class Component {
+  /**@type {Node} */
+  node = null;
+  constructor(data) {
+    Object.assign(this, data);
+    this.create();
+  }
+
+  create() {
+    throw new Error("Method not implemented");
+  }
 }
 
-const renderTopicOnPageHeader = async () => {
-  try
-  {
-     let { data: topicList } = await axios.get("/data/topic-list.json");
-     
-     if(!Array.isArray(topicList))
-     {
-        let err =  new Error("topicList not is an array");
-        err.topicList = topicList;
-        throw err;
-     }
-    
-     const [ pageHeader ] = document.getElementsByClassName("page-header");
-  
-     for(const topic of topicList)
-     {
-       pageHeader.appendChild(renderTopicButton(topic));
-     }
+class TopicItem extends Component {
+  content = "";
+  url = "";
+
+  constructor(data = TopicItemDataObject) {
+    super(data);
   }
-  catch(err)
-  {
-    console.error("Get topic list not success",err);   
+
+  create() {
+    this.node = document.createElement("a");
+    this.node.className = "btn";
+    this.node.innerHTML = this.content;
+    this.node.href = this.url;
+  }
+}
+
+//#endregion
+
+const renderTopicOnPageHeader = async () => {
+  try {
+    let { data: topicList } = await axios.get("/data/topic-list.json");
+
+    if (!Array.isArray(topicList)) {
+      let err = new Error("topicList not is an array");
+      err.topicList = topicList;
+      throw err;
+    }
+
+    const [pageHeader] = document.getElementsByClassName("page-header");
+
+    for (const topic of topicList) {
+      let topicItem = new TopicItem(topic);
+      pageHeader.appendChild(topicItem.node);
+    }
+  }
+  catch (err) {
+    console.error("Get topic list not success", err);
   }
 }
 
