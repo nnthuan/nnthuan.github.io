a46a4f18561b9c6de78b97e732b9f6998446829b
Nguyễn Như Thuận <nguyennhuthuanse@gmail.com>
2021-07-10 15:33:35 +0700

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,17 @@
+const renderTopicOnPageHeader = async () => {
+  try
+  {
+     let { data } = await axios.get("https://raw.githubusercontent.com/nnthuan/nnthuan.github.io/main/data/topic-list.json");
+     console.log(data);
+  }
+  catch(err)
+  {
+    console.error("Get topic list not success",err);   
+  }
+}
+
+const main = async () => {
+  await renderTopicOnPageHeader();
+}
+
 var pageHeader = document.getElementsByClassName("page-header");
-axios.get("https://raw.githubusercontent.com/nnthuan/nnthuan.github.io/main/data/topic-list.json")
-.resolve(topicList => console.log(topicList));
